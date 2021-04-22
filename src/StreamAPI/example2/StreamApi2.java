44c5f9812ac58882af5fd62eee36bd94f3a22b3d
OTMANEER <otmane.erragragui@gmail.com>
2021-04-22 05:31:41 +0200

    Streams

--- src/StreamAPI/example2/StreamApi2.java
+++ src/StreamAPI/example2/StreamApi2.java
@@ -1,4 +1,19 @@
 package StreamAPI.example2;
 
+import java.util.List;
+
 public class StreamApi2 {
+    public static void main(String[] args) {
+        List<String> list = List.of("10","201","jeaea","OTMANE");
+
+        System.out.println(list.stream()
+                .map(String::length)
+                .reduce(0, Integer::sum));
+        // reduce  is a terminal operator,
+        System.out.println(list.stream()
+                .mapToInt(s -> s.length())
+                .sum());
+
+
+    }
 }
