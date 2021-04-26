68d2a5ab798bbac6c77298a18048949c9037fe83
OTMANEER <otmane.erragragui@gmail.com>
2021-04-26 18:48:24 +0200

    Streams

--- src/StreamAPI/peeketc/pee.java
+++ src/StreamAPI/peeketc/pee.java
@@ -6,7 +6,7 @@ public class pee {
     public static void main(String[] args) {
         List<Integer> list=List.of(12,21,11,99,28,53);
         list.stream()
-                .peek(s-> System.out.println(s))
+                .peek(System.out::println)
                 .forEach(System.out::println);
     }
 }
