c02a81f0415d290f1bf5f93ffda677541ea1d8b9
OTMANEER <otmane.erragragui@gmail.com>
2021-05-02 01:25:29 +0200

    Streams

---
+++ src/testGeneral/one.java
@@ -0,0 +1,18 @@
+package testGeneral;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class one {
+    public static void main(String[] args) {
+        List<List<String>> sentences = new ArrayList<List<String>>();
+
+
+        NumberFormatException;
+        FileNotFoundException
+
+
+    }
+
+}
